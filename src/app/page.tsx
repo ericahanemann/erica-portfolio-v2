aa8f817fc0b2add5048cc1bd91cd1de586c25ca8
Érica <ericavhanemann@gmail.com>
2024-06-25 17:58:32 -0300

    feat: add full name shining effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Link from 'next/link'
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>home page</div>
+      <section className={styles.hero}>
+        <div>FRONTEND DEVELOPER</div>
+        <h1 className={styles.name}>
+          ERICA <br /> VANESSA <br /> HANEMANN
+        </h1>
+      </section>
       <Link href="/project">project page</Link>
     </main>
   );
