717542e9bf414b339576e331e98e33daafa2792c
Érica <ericavhanemann@gmail.com>
2024-06-26 15:34:05 -0300

    feat: add hero section design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,36 @@
 import Link from "next/link";
 import styles from "./page.module.css";
 
+import { GrLinkedin, GrGithub } from "react-icons/gr";
+
 export default function Home() {
   return (
     <main className={styles.main}>
       <section className={styles.hero}>
-        <div>FRONTEND DEVELOPER</div>
+        <div className={styles.heroInfo}>
+          <h4 className={styles.heroInfoTitle}>FRONTEND DEVELOPER</h4>
+          <p className={styles.heroInfoText}>
+            A passionate developer on a mission to blend design and
+            functionality, creating immersive frontend experiences that leave a
+            LONG lasting impression
+          </p>
+          <div className={styles.heroInfoSocials}>
+            <a
+              href="https://github.com/ericahanemann"
+              target="_blank"
+              className={styles.heroSocialIconLink}
+            >
+              <GrGithub className={styles.heroSocialIcon}></GrGithub>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/erica-hanemann/"
+              target="_blank"
+              className={styles.heroSocialIconLink}
+            >
+              <GrLinkedin className={styles.heroSocialIcon}></GrLinkedin>
+            </a>
+          </div>
+        </div>
         <h1 className={styles.name}>
           ERICA <br /> VANESSA <br /> HANEMANN
         </h1>
